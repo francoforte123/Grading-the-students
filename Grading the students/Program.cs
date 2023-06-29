1aab689fa676255c05d9871deeeaf9461d610c1f
rober <rober@DESKTOP-MN9AKPU>
2023-06-29 14:20:05 +0200

    Aggiungere i file di progetto.

---
+++ Grading the students/Program.cs
@@ -0,0 +1,40 @@
+﻿using Grading_the_students;
+
+Console.WriteLine("creo 10 studenti con nomi e voto");
+Student students = new();
+
+Student student1 = new Student("Marco", 26);
+Student student2 = new Student("Luca", 30);
+Student student3 = new Student("Anna", 28);
+Student student4 = new Student("GianMarco", 19);
+Student student5 = new Student("Franco", 21);
+Student student6 = new Student("Lucia", 20);
+Student student7 = new Student("Alice", 26);
+Student student8 = new Student("Francesca", 29);
+Student student9 = new Student("Emanuele", 23);
+Student student10 = new Student("Mario", 22);
+
+List<Student> StudentList = new()
+{
+    student1, student2, student3, student4, student5,
+    student6, student7, student8, student9, student10,
+};
+
+foreach(Student name in StudentList)
+{
+    Console.WriteLine(name.GetStudent());
+}
+
+double TotalGrade = 0;
+
+foreach(Student student in StudentList)
+{
+    TotalGrade = (TotalGrade += student.GradeStudent)/StudentList.Count;
+}
+
+Console.WriteLine("\nla media totale è " + TotalGrade);
+
+Console.Write("\nlo/a studente/ssa con il voto più alto è: ");
+
+string NameStudent = students.GetNameStudentWithGradeMax(StudentList);
+Console.WriteLine("nome: " + NameStudent);
---
+++ Grading the students/Student.cs
@@ -0,0 +1,42 @@
+﻿using System.Globalization;
+
+namespace Grading_the_students
+{
+    internal class Student
+    {
+        public string NameStudent;
+        public int GradeStudent;     
+
+        public Student()
+        {
+        }
+
+        public Student(string nameStudent, int gradeStudent)
+        {
+            NameStudent = nameStudent;
+            GradeStudent = gradeStudent;
+        }
+
+        public string GetStudent()
+        {
+            return "nome dello studente: " + NameStudent + ", voto: " + GradeStudent;
+        }
+
+        public string GetNameStudentWithGradeMax(List<Student> ListOfStudents)
+        {
+            string nameStudent = "";
+            int maxGrade = ListOfStudents[0].GradeStudent;
+
+            foreach (var student in ListOfStudents)
+            {
+                if (student.GradeStudent > maxGrade)
+                {
+                    maxGrade = student.GradeStudent;
+                    nameStudent = student.NameStudent;
+                }
+            }
+            return nameStudent;
+        }
+
+    }
+}
